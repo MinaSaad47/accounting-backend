c66e974f43afa2da956e382f49ca78fbfd0ae2a9
MinaSaad47 <MinaSaad-47@outlook.com>
2022-08-30 15:51:26 +0200

    [dependency] dotenvy

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use accounting_backend::{database, routes};
 
 #[launch]
 fn rocket() -> _ {
+    if cfg!(debug_assertions) {
+        dotenvy::dotenv().ok();
+    }
+
     rocket::build()
         .attach(database::stage())
         .attach(routes::stage())
