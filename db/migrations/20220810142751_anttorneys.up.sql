a8a5e41002fd38eba435180d34e11e544fa0b657
MinaSaad47 <MinaSaad-47@outlook.com>
2022-09-05 14:05:38 +0200

    [migrations] delete unused

--- db/migrations/20220810142751_anttorneys.up.sql
+++
@@ -1,6 +0,0 @@
--- Add up migration script here
-CREATE TABLE IF NOT EXISTS anttorneys (
-    id BIGSERIAL PRIMARY KEY,
-    number VARCHAR NOT NULL,
-    company_id BIGINT REFERENCES companies(id)
-)
